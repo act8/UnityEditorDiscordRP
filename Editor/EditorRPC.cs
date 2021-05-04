4fec2c3c2e7461f6b84c284262587ac7696abc3b
AuM0b <aum0b@act8team.com>
2021-05-04 12:18:47 +0700

    Cleanup

--- Editor/EditorRPC.cs
+++ Editor/EditorRPC.cs
@@ -1,127 +1,186 @@
-﻿#if UNITY_EDITOR
-using Discord;
+﻿using Discord;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-#region Editor RPC
 namespace EditorRPC
 {
-    #region Activity Extension
-    /// <summary>
-    /// Activity Extension, ported from act8.DiscordActivityManager
-    /// </summary>
-    public static class ActivityExtension
-    {
-        public static Activity AddState(this Activity activity, string value)
-        {
-            activity.State = value;
-            return activity;
-        }
+	#region Extensions
 
-        public static Activity AddDetails(this Activity activity, string value)
-        {
-            activity.Details = value;
-            return activity;
-        }
+	#region Activity Extension
 
-        public static Activity AddTimestamps(this Activity activity, ActivityTimestamps value)
-        {
-            activity.Timestamps = value;
-            return activity;
-        }
+	/// <summary>
+	/// Activity Extension, ported from act8.DiscordActivityManager
+	/// </summary>
+	public static class ActivityExtension
+	{
+		public static Activity AddState(this Activity activity, string value)
+		{
+			activity.State = value;
+			return activity;
+		}
 
-        public static Activity AddAssets(this Activity activity, ActivityAssets value)
-        {
-            activity.Assets = value;
-            return activity;
-        }
-    }
-    #endregion
+		public static Activity AddDetails(this Activity activity, string value)
+		{
+			activity.Details = value;
+			return activity;
+		}
 
-    #region Editor RPC
-    [InitializeOnLoad]
-    public static class EditorRPC
-    {
-        public static Discord.Discord discord { get; private set; }
-        static ActivityManager ActivityManager { get; set; }
-        static readonly long CLIENT_ID = 838427392871366667;
+		public static Activity AddTimestamps(this Activity activity, ActivityTimestamps value)
+		{
+			activity.Timestamps = value;
+			return activity;
+		}
 
-        static Activity CurrentActivity;
+		public static Activity AddAssets(this Activity activity, ActivityAssets value)
+		{
+			activity.Assets = value;
+			return activity;
+		}
+	}
+	#endregion
 
-        static string AppName;
-        static string SceneName;
-        static long StartTime;
+	#region Timestamps Extension
 
-        static EditorRPC()
-        {
-            UnityIntendedDelay();
-        }
-        static async void UnityIntendedDelay()
-        {
-            await Task.Delay(1000);
-            Init();
-        }
-        public static void Init()
-        {
-            StartTime = DateTimeOffset.Now.ToUnixTimeSeconds();
-            AppName = Application.productName;
-            SceneName = EditorSceneManager.GetActiveScene().name;
-            SetupDiscord();
+	/// <summary>
+	/// Activity Timestamps Extension, ported from act8.DiscordActivityManager
+	/// </summary>
+	public static class ActivityTimestampsExtension
+	{
+		public static ActivityTimestamps AddStart(this ActivityTimestamps timestamps, long start)
+		{
+			timestamps.Start = start;
+			return timestamps;
+		}
 
-            // Update Events
-            EditorApplication.update += Update;
-            EditorSceneManager.sceneOpened += SceneLoadedCallback;
+		public static ActivityTimestamps AddEnd(this ActivityTimestamps timestamps, long end)
+		{
+			timestamps.End = end;
+			return timestamps;
+		}
+	}
+	#endregion
 
-        }
+	#region Assets Extension
 
-        static void SetDiscordActivity(Activity activity)
-        {
-            if (discord == null) return;
+	/// <summary>
+	/// Activity Assets Extension, ported from act8.DiscordActivityManager
+	/// </summary>
+	public static class ActivityAssetsExtension
+	{
+		public static ActivityAssets AddLargeImage(this ActivityAssets assets, string largeImage)
+		{
+			assets.LargeImage = largeImage;
+			return assets;
+		}
 
-            ActivityManager.UpdateActivity(activity, (result) =>
-            {
-                if (result == Result.Ok) CurrentActivity = activity;
-                else Debug.LogWarning("Discord was unable to set activity: " + result.ToString());
-            }
-            ); 
-        }
+		public static ActivityAssets AddLargeText(this ActivityAssets assets, string largeText)
+		{
+			assets.LargeText = largeText;
+			return assets;
+		}
 
-        static void UpdateActivity()
-        {
-            SetDiscordActivity(new Activity().AddState($"on scene {SceneName}").AddDetails($"In project {AppName}").AddTimestamps(new ActivityTimestamps() { Start = StartTime }).AddAssets(new ActivityAssets() { LargeImage = "unity-black", LargeText = AppName }));
-        }
+		public static ActivityAssets AddSmallImage(this ActivityAssets assets, string smallImage)
+		{
+			assets.SmallImage = smallImage;
+			return assets;
+		}
 
-        static void SetupDiscord()
-        {
-            try
-            {
-                discord = new Discord.Discord(CLIENT_ID, (ulong)CreateFlags.NoRequireDiscord);
-                ActivityManager = discord.GetActivityManager();
-                UpdateActivity();
-            }
-            catch (Exception e)
-            { Debug.LogWarning($"Failed to initiate Discord session: {e.Message}"); }
-        }
+		public static ActivityAssets AddSmallText(this ActivityAssets assets, string smallText)
+		{
+			assets.SmallText = smallText;
+			return assets;
+		}
+	}
+	#endregion
 
-        static void SceneLoadedCallback(Scene scene, OpenSceneMode openMode)
-        {
-            SceneName = scene.name;
-            UpdateActivity();
-        }
+	#endregion
 
-        private static void Update()
-        {
-            if (discord != null)
-                discord.RunCallbacks();
-        }
-    }
-    #endregion
+	#region Editor RPC
+
+	[InitializeOnLoad]
+	public static class EditorRPC
+	{
+		static Discord.Discord DiscordInstance;
+		static ActivityManager ActivityManager;
+		static readonly long CLIENT_ID = 838427392871366667;
+
+		static string AppName;
+		static string SceneName;
+		static long StartTime;
+
+		static EditorRPC()
+		{
+			AsyncInit();
+		}
+
+		static async void AsyncInit()
+		{
+			await Task.Delay(1000);
+			Init();
+		}
+
+		private static void Init()
+		{
+			StartTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+			AppName = Application.productName;
+			SceneName = SceneManager.GetActiveScene().name;
+
+			SetupDiscord();
+
+			EditorApplication.update += Update;
+			EditorSceneManager.sceneOpened += OnSceneOpened;
+		}
+
+		private static void SetupDiscord()
+		{
+			try
+			{
+				DiscordInstance = new Discord.Discord(CLIENT_ID, (ulong)CreateFlags.NoRequireDiscord);
+				ActivityManager = DiscordInstance.GetActivityManager();
+				UpdateActivity();
+			}
+			catch (Exception e)
+			{
+				Debug.LogWarning($"Failed to initiate Discord session: {e.Message}");
+			}
+		}
+
+		private static void Update()
+		{
+			if (DiscordInstance != null) DiscordInstance.RunCallbacks();
+		}
+
+		private static void OnSceneOpened(Scene scene, OpenSceneMode openMode)
+		{
+			AppName = Application.productName;
+			SceneName = scene.name;
+			UpdateActivity();
+		}
+
+		private static void UpdateActivity()
+		{
+			SetDiscordActivity(new Activity()
+				.AddState($"on scene {SceneName}")
+				.AddDetails($"In project {AppName}")
+				.AddTimestamps(new ActivityTimestamps().AddStart(StartTime))
+				.AddAssets(new ActivityAssets().AddLargeImage("unity-black").AddLargeText(AppName)));
+		}
+
+		private static void SetDiscordActivity(Activity activity)
+		{
+			if (DiscordInstance == null) return;
+
+			ActivityManager.UpdateActivity(activity, (result) =>
+			{
+				if (result != Result.Ok) Debug.LogWarning("Discord was unable to set activity: " + result.ToString());
+			}
+			);
+		}
+	}
+
+	#endregion
 }
-#endregion
-#endif
